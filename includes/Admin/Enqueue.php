9332131f04f140d21ce253e63a490bc32d7c9d5d
adnan159 <osmanhaider159@gmail.com>
2023-06-08 02:23:25 +0600

    Add: Laravel mix added

---
+++ includes/Admin/Enqueue.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AsCode\WooCalculator\Admin;
+
+class Enqueue {
+
+    public function __construct() {
+        add_action( 'admin_enqueue_scripts', [$this, 'admin_script'], 10, 1 );
+
+    }
+
+    /**
+     * Load script
+     *
+     * @param $page
+     * @return void
+     */
+    public function admin_script( $page ) {
+        if( $page === 'toplevel_page_ascode-woo-calculator' ) {
+            wp_enqueue_script( 'ascode-woo-calculator-dashboard', ASC_WOO_CALCULATOR_ASSETS .'/admin/js/dashboard.js', [], false, true );
+        }
+    }
+}
