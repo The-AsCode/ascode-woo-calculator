a4b32577d5987a146b10671489d5d539c43e73ee
adnan159 <mac@MACs-Mac-mini.local>
2023-06-21 14:38:30 +0600

    Add: Frontend Ajax loaded

--- assets/front/src/view/App.js
+++ assets/front/src/view/App.js
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 export default function App() {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        let data = {
+            'action': 'ascode_load_calculator_info_action',
+            '_ajax_nonce': output_ajax_object.nonce,
+        };
+
+        jQuery.post(output_ajax_object.ajax_url, data, (response) => {
+            console.log(response.data);
+            setData('response.data');
+            // alert(response.data.message);
+        });
+
+    }, []);
     return (
         <div className='border'>Hello from js</div>
     )
--- includes/Front/Enqueue.php
+++ includes/Front/Enqueue.php
@@ -16,6 +16,14 @@ class Enqueue
     public function frontend_scipts()
     {
         wp_enqueue_style('ascode-woo-calculator-frontend', ASC_WOO_CALCULATOR_ASSETS . '/front/css/output.css');
-        wp_enqueue_script('ascode-frontend', ASC_WOO_CALCULATOR_ASSETS . '/front/js/frontend.js', [], null, true);
+        wp_enqueue_script('ascode-woo-calculator-frontend', ASC_WOO_CALCULATOR_ASSETS . '/front/js/frontend.js', ['jquery'], null, true);
+
+        wp_localize_script(
+            'ascode-woo-calculator-frontend',
+            'output_ajax_object',
+             [
+                 'ajax_url' => admin_url( 'admin-ajax.php' ),
+                 'ajax_nonce' => wp_create_nonce( 'ascode-calculator-show-calculator' )
+             ] );
     }
 }
