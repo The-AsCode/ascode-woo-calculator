2d5d805a8bbd6b95596fb085f8703fb41d3a15cc
adnan159 <mac@MACs-Mac-mini.local>
2023-06-22 12:06:02 +0600

    Add: Single product meta added

--- includes/Admin/AdminAction.php
+++ includes/Admin/AdminAction.php
@@ -4,18 +4,36 @@ namespace AsCode\WooCalculator\Admin;
 
 class AdminAction {
     public function __construct(){
-        add_action('woocommerce_product_options_general_product_data', [$this, 'add_custom_field_to_product' ]);
+        add_action('woocommerce_product_options_general_product_data', [ $this, 'ascode_custom_field_to_product' ]);
+        add_action('woocommerce_process_product_meta', [ $this, 'woo_calculator_save_custom_field' ]);
     }
 
-    public function add_custom_field_to_product() {
+    /**
+     * Custom field add in single product
+     *
+     * @return void
+     */
+    public function ascode_custom_field_to_product() {
         echo '<div class="options_group">';
         woocommerce_wp_text_input(
             array(
-                'id' => 'custom_field',
-                'label' => 'Custom Field',
-                'description' => 'Enter a value for the custom field.',
+                'id'            => 'woo_calculator_value',
+                'label'         => 'Capacity',
+                'type'          => 'number',
+                'description'   => 'Enter a value number value in Watt',
             )
         );
         echo '</div>';
+    }
+
+    /**
+     * Save custom field value from product page.
+     *
+     * @param $post_id
+     * @return void
+     */
+    function woo_calculator_save_custom_field($post_id) {
+        $custom_field_value = isset($_POST['woo_calculator_value']) ? sanitize_text_field($_POST['woo_calculator_value']) : '';
+        update_post_meta($post_id, 'woo_calculator', $custom_field_value);
     }
 }
